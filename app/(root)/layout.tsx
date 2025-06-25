870d8d807c69ca8d1fd0c2f2ccd2490478f0a4d5
Dmitriy Latkin <lda020504@gmail.com>
2025-06-25 15:24:35 +0700

    добавление root и dashboard layouts

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Nunito } from "next/font/google";
 
 import { Header } from "@/components/shared";
 
-import "../globals.css";
 import React from "react";
-
-const nunito = Nunito({
-  subsets: ["cyrillic"],
-  variable: "--font-nunito",
-  weight: ["400", "500", "600", "700", "800", "900"],
-});
 
 export const metadata: Metadata = {
   title: "Fake Pizza | Главная",
@@ -23,11 +15,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ru">
-      <body className={`${nunito.variable} antialiased`}>
-        <Header />
-        {children}
-      </body>
-    </html>
+    <>
+      <Header />
+      <main>{children}</main>
+    </>
   );
 }
